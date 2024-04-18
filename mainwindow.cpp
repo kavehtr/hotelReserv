86e3d930de5f04302758da489eaff5aa08ccd04a
kavehtr <team-neptune@outlook.com>
2024-04-18 09:59:10 +0330

    removed listview

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,7 @@
 #include "ui_mainwindow.h"
 
 // qt built in imports
-#include <QTableView>
-#include <QStandardItemModel>
-#include <QStringList>
+
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -14,30 +12,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Declare and initialize the verticalHeader variable as a QStringList
-    QStringList verticalHeader;
-    verticalHeader << "Row 1" << "Row 2" << "Row 3";
 
-    // Create a QStandardItemModel and set its horizontal and vertical headers
-    QStandardItemModel *model = new QStandardItemModel();
-    QStringList horizontalHeader;
-    horizontalHeader << "Column 1" << "Column 2" << "Column 3";
-    model->setHorizontalHeaderLabels(horizontalHeader);
-    model->setVerticalHeaderLabels(verticalHeader);
-
-    // Create a QTableView widget and set the model
-    QTableView *tableView = new QTableView();
-    tableView->setModel(model);
-
-    // Optionally, set other properties like visibility of the vertical header, grid style, etc.
-    tableView->verticalHeader()->setVisible(false);
-    tableView->verticalHeader()->setDefaultSectionSize(10);
-    tableView->setShowGrid(false);
-
-    // Add data to the table by creating QStandardItem instances and appending them to the model
-    QStandardItem *col1 = new QStandardItem("Text 1");
-    QStandardItem *col2 = new QStandardItem("Text 2");
-    model->appendRow(QList<QStandardItem*>() << col1 << col2);
 
  /* ######################################################### Styles #######################################################*/
 
@@ -117,13 +92,6 @@ MainWindow::MainWindow(QWidget *parent)
         "icon: url(:/new/prefix1/icons8-living-room-24.png);"
         );
 
-    ui->pushButton->setStyleSheet(
-        "background-color: #2e2e2e;"
-        "border-radius: 10px;"
-        "#pushButton:hover {"
-        "background-color: white;"
-        "}"
-        );
 
     ui->roomsList->setStyleSheet(
         "font-size: 15px;"
